5ac2e1206fc6812573bd5ffd29dfddddf5ccf48d
evgeniy-kulikov <redstepway@gmail.com>
2021-11-11 22:45:39 +0300

    finish task_01, task_02

--- Task_01.py
+++ Task_01.py
@@ -3,3 +3,15 @@
 
 # Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
 # Об окончании ввода данных свидетельствует пустая строка.
+
+with open('less_01_text.txt', 'w') as my_file:
+    line = input('Введите текст: ')
+    while line:
+        my_file.writelines(line + '\n')
+        line = input(f'Введите текст: ')
+        if not line:
+            break
+
+with open('less_01_text.txt', 'r') as my_file:
+    content = my_file.read()
+    print(f'\nСодержимое файла:\n{content}')
--- Task_02.py
+++ Task_02.py
@@ -3,3 +3,18 @@
 
 # Создать текстовый файл (не программно), сохранить в нем несколько строк,
 # выполнить подсчет количества строк, количества слов в каждой строке.
+
+with open('less_02_text.txt', 'r') as my_file:
+    content = my_file.read()
+    print(f'Содержимое файла:\n{content}\n')
+with open('less_02_text.txt', 'r') as my_file:
+    content = my_file.readlines()
+    print(f'Количество строк в файле: {len(content)}')
+with open('less_02_text.txt', 'r') as my_file:
+    content = my_file.readlines()
+    for i in range(len(content)):
+        print(f'В строке #{i + 1} кол-во символов: {len(content[i])}')
+with open('less_02_text.txt', 'r') as my_file:
+    content = my_file.read()
+    content = content.split()
+    print(f'Общее кол-во слов: {len(content)}')
