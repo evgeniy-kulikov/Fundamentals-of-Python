59face7bf85daaa0b2073d806f194910850038f9
evgeniy-kulikov <redstepway@gmail.com>
2021-11-12 23:27:23 +0300

    add task_03

--- Task_03.py
+++ Task_03.py
@@ -5,13 +5,18 @@
 # Определить, кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников.
 # Выполнить подсчет средней величины дохода сотрудников.
 
-with open('Task_03_text.txt', 'r') as my_file:
-    sal = []
-    poor = []
-    my_list = my_file.read().split('\n')
-    for i in my_list:
+with open('Task_03_text.txt', 'r', encoding='utf-8') as my_file:
+    salary = 0
+    count = 0
+    low_salary = []
+    content = my_file.read().split('\n')
+    for i in content:
         i = i.split()
-        if int(i[1]) < 20000:
-           poor.append(i[0])
-        sal.append(i[1])
-print(f'Оклад меньше 20.000 {poor}, средний оклад {sum(map(int, sal)) / len(sal)}')
+        if int(i[1]) < 20_000:
+            low_salary.append(i[0])
+        salary += int(i[1])
+        count += 1
+print('Оклад меньше 20 000:')
+for i in low_salary:
+    print(i)
+print(f'\nсредний оклад сотрудников:\n{round((salary / count), 2)}')
