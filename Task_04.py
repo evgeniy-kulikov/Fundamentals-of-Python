c59333fd40bdef1ddf84744bda02d13d56c91ea6
evgeniy-kulikov <redstepway@gmail.com>
2021-11-13 21:25:40 +0300

    add file for task_04"

--- Task_04.py
+++ Task_04.py
@@ -9,3 +9,14 @@
 # Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные.
 # При этом английские числительные должны заменяться на русские.
 # Новый блок строк должен записываться в новый текстовый файл.
+
+dict_rus = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+new_file = []
+with open('Task_04_1_text.txt', 'r', encoding='utf-8') as my_file:
+    for i in my_file:
+        i = i.split(' ', maxsplit=1)
+        new_file.append(dict_rus[i[0]] + ' ' + i[1])
+    print(''.join(new_file))
+
+with open('Task_04_2_text.txt', 'w', encoding='utf-8') as my_file:
+    my_file.writelines(new_file)
