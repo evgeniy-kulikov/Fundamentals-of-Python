33445c1368d559a903f68ef3d800014e7d6de75c
evgeniy-kulikov <redstepway@gmail.com>
2021-11-15 11:51:09 +0300

    add task_07

--- Task_07.py
+++ Task_07.py
@@ -1,6 +1,7 @@
 """ Homework for the Lesson_05 """
-""" Task 07 """
+import json
 
+""" Task 07 """
 # Создать (не программно) текстовый файл, в котором каждая строка должна содержать данные о фирме: название,
 # форма собственности, выручка, издержки.
 # Пример строки файла: firm_1 ООО 10000 5000.
@@ -15,32 +16,22 @@
 #
 # Подсказка: использовать менеджеры контекста.
 
+companies = {}
+count = 0  # число успешных компаний
+pos_sum = 0  # сумма дохода  успешных компаний
+with open('Task_07_text.txt', 'r', encoding='utf-8') as file:
+    file_lines = file.readlines()
+    for line in file_lines:
+        data = line.split()
+        profit = float(data[2]) - float(data[3])  # определяем доход каждой компании
+        companies.update({data[0]: profit})  # записываем в словарь доход компании
+        if profit > 0:  # готовим данные о среднем доходе успешных компаний
+            count += 1
+            pos_sum += profit
 
-# import json
-# profit = {}
-# pr = {}
-# prof = 0
-# prof_aver = 0
-# i = 0
-# with open('file_7.txt', 'r') as file:
-#     for line in file:
-#         name, firm, earning, damage = line.split()
-#         profit[name] = int(earning) - int(damage)
-#         if profit.setdefault(name) >= 0:
-#             prof = prof + profit.setdefault(name)
-#             i += 1
-#     if i != 0:
-#         prof_aver = prof / i
-#         print(f'Прибыль средняя - {prof_aver:.2f}')
-#     else:
-#         print(f'Прибыль средняя - отсутсвует. Все работают в убыток')
-#     pr = {'средняя прибыль': round(prof_aver)}
-#     profit.update(pr)
-#     print(f'Прибыль каждой компании - {profit}')
-#
-# with open('file_7.json', 'w') as write_js:
-#     json.dump(profit, write_js)
-#
-#     js_str = json.dumps(profit)
-#     print(f'Создан файл с расширением json со следующим содержимым: \n '
-#           f' {js_str}')
+average_profit = pos_sum / count  # формируем средний доход  успешных компаний
+result = [companies, {'average_profit': average_profit}]
+
+with open('Task_07_result.json', 'w', encoding='utf-8') as file:
+    json.dump(result, file)
+    print(f'Создан файл Task_07_result.json со следующим содержимым:\n{result}')
