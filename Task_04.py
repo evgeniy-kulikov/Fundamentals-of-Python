e60ec63a2a2df91b87ec52ffdb14e74c3c1e9235
evgeniy-kulikov <redstepway@gmail.com>
2021-11-19 10:38:35 +0300

    add task_04

--- Task_04.py
+++ Task_04.py
@@ -8,3 +8,63 @@
 # При значении скорости свыше 60 (TownCar) и 40 (WorkCar) должно выводиться сообщение о превышении скорости.
 # Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к атрибутам, выведите результат.
 # Выполните вызов методов и также покажите результат.
+
+class Car:
+    def __init__(self, speed, color, name, is_police=False):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+# Методы класса: движение поворот, скорость
+    def go(self):
+        print('Start of movement')
+
+    def stop(self):
+        print('Stop of movement')
+
+    def turn(self, direction):  # метод принимает значение
+        print(f'Change of direction to the {direction}')
+
+    def show_speed(self):
+        print(f'Current speed {self.speed} km/h')
+
+
+class TownCar(Car):  # дочерний класс с переопределением значения "show_speed"
+    def show_speed(self):
+        super().show_speed()
+        if self.speed > 60:
+            print('Travel speed > 60 km/h. Over speed!')
+
+
+class WorkCar(Car):
+    def show_speed(self):
+        super().show_speed()
+        if self.speed > 40:
+            print('Travel speed > 40 km/h. Over speed!')
+
+
+class SportCar(Car):
+    def show_speed(self):
+        super().show_speed()
+        if self.speed > 160:
+            print('Be careful!')
+
+
+class PoliceCar(Car):
+    def __init__(self, speed, color, name):
+        super().__init__(speed, color, name, True)  # Переопределяем констркутор для изменения свойства "is_police"
+
+
+car_town = TownCar(80, 'Blue', 'Opel')
+car_work = WorkCar(50, 'Green', 'Renault')
+car_sport = SportCar(180, 'Red', 'Renault')
+car_police = PoliceCar(90, 'Special paint', 'Company car')
+
+car_town.show_speed()
+car_town.speed = 60  # изменяем скорость
+car_town.show_speed()
+print(f'Car model "{car_work.name}", color "{car_work.color}"\n')
+car_police.turn('left\n')
+car_sport.go()
+car_sport.show_speed()
